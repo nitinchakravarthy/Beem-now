284d9c3182ad2aa2b98b2d4d9b5a4881215c5e4d
Mohinish Chatterjee <maxoutbreak@gmail.com>
2019-11-09 19:14:33 -0600

    Added Gender label to signup page

---
+++ src/components/ResetPassword.js
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Redirect } from 'react-router-dom';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Beem Now
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function ResetPassword() {
+  const classes = useStyles();
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const [resetSuccess, setResetSuccess] = useState(false);
+
+  const validatePassword = (event) => {
+      let name = event.target.name
+      let value = event.target.value
+      setPassword(value)
+      if(event.target.value==''){
+          setPasswordError('Password Required')
+      }
+      else{
+          var pass = event.target.value;
+          var reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,32}$/;
+          var test = reg.test(pass);
+          if (test) {
+              setPasswordError('')
+          } else{
+              setPasswordError('Password should contain a digit,small letter,capital letter and must be between 8 to 32 characters')
+          }
+      }
+    }
+  const validatePasswordComfirmation = (event) => {
+      let name = event.target.name
+      let value = event.target.value 
+      var pass = event.target.value;
+      if (pass === password) {
+          setConfirmPasswordError('')
+      } else{
+          setConfirmPasswordError('Passwords do not match')
+      }
+  }
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      const data = new FormData(event.target);
+      if(data.get('password') === ''){
+        setPasswordError('Password can not be empty.')
+      } else if(data.get('confirmPassword') === ''){
+        setConfirmPasswordError('Please confirm your password.')
+      } else {
+        setPasswordError('')
+        setConfirmPasswordError('')
+        // send new credentials to backend
+        // fetch('/api/form-submit-url', {
+        //   method: 'POST',
+        //   body: data,
+        // });
+        setResetSuccess(true);
+      }
+      
+  }
+  return (
+    <div>
+      {resetSuccess ? <Redirect to="/"/> : null}
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Reset Password
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  error={passwordError !== ''}
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="New Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  helperText = {passwordError}
+                  onChange = {validatePassword}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  error = {confirmPasswordError !== ''}
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="password"
+                  id="confirm-password"
+                  autoComplete="confirm-password"
+                  helperText = {confirmPasswordError}
+                  onChange = {validatePasswordComfirmation}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Reset
+            </Button>
+          </form>
+        </div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
+    </div>
+  );
+}
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,6 +12,10 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
 
 function Copyright() {
   return (
@@ -57,6 +61,14 @@ export default function SignUp() {
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [confirmPasswordError, setConfirmPasswordError] = useState('');
+  const [gender, setGender] = useState(''); 
+
+  // Parameters for UI effect of Gender label when in focus
+  const genderLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(genderLabel.current.offsetWidth);
+  }, []);
 
   const validateEmailAddress = (event) =>{
       let name = event.target.name
@@ -66,12 +78,12 @@ export default function SignUp() {
       }
       else{
           var email = event.target.value;
-          var reg = /^[a-zA-Z0-9/_/g]+@[a-zA-Z0-9]+\.[A-Za-z]+(\.[A-Za-z]+)?$/;
+          var reg = /^[a-zA-Z0-9/_/g]+@tamu\.edu$/;
           var test = reg.test(email);
           if (test) {
               setEmailError('')
           } else{
-              setEmailError('Not a valid email address')
+              setEmailError('Please enter a valid tamu email.')
           }
       }
   }
@@ -84,7 +96,7 @@ export default function SignUp() {
       }
       else{
           var pass = event.target.value;
-          var reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,32}$/;
+          var reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,32}$/;
           var test = reg.test(pass);
           if (test) {
               setPasswordError('')
@@ -96,23 +108,27 @@ export default function SignUp() {
   const validatePasswordComfirmation = (event) => {
       let name = event.target.name
       let value = event.target.value 
-      var pass = event.target.value;
+      var pass = event.target.value
       if (pass === password) {
           setConfirmPasswordError('')
       } else{
           setConfirmPasswordError('Passwords do not match')
       }
   }
-  // const handleSubmit = (event) => {
-  //     event.preventDefault();
-  //     const data = new FormData(event.target);
+  const handleGenderChange = event => {
+    setGender(event.target.value)
+  }
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      const data = new FormData(event.target)
+      console.log(gender)
 
-  //     fetch('/api/form-submit-url', {
-  //       method: 'POST',
-  //       body: data,
-  //     });
+      // fetch('/api/form-submit-url', {
+      //   method: 'POST',
+      //   body: data,
+      // });
       
-  // }
+  }
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -123,7 +139,7 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form}  noValidate>
+        <form className={classes.form}  onSubmit={handleSubmit} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -147,6 +163,24 @@ export default function SignUp() {
                 name="lastName"
                 autoComplete="lname"
               />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl variant="outlined" id = "gender-form-control" fullWidth className={classes.formControl}>
+                <InputLabel ref={genderLabel} id="gender-label" required>
+                  Gender
+                </InputLabel>
+                <Select
+                  labelId="gender-label"
+                  id="gender"
+                  labelWidth={labelWidth}
+                  value={gender}
+                  onChange={handleGenderChange}
+                >
+                  <MenuItem value={'Male'}>Male</MenuItem>
+                  <MenuItem value={'Female'}>Female</MenuItem>
+                  <MenuItem value={'Other'}>Prefer not to disclose</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
             <Grid item xs={12}>
               <TextField
